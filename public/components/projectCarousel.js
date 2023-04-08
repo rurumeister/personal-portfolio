3133d0bc9ca1441008fafe9ca3e6b98018d75c87
Elroy Chua <elroymx@gmail.com>
2023-04-08 09:23:35 +0800

    added material-ui dependency; added responsive logic for projectCarousel; added media pages

--- public/components/projectCarousel.js
+++ public/components/projectCarousel.js
@@ -9,9 +9,11 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-const ProjectCarousel = ({ screenWidth }) => {
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const ProjectCarouselWeb = () => {
   return (
-    <div style={{ display: screenWidth < 600 ? "none" : "block" }}>
+    <div>
       <Swiper
         style={{
           width: "100%",
@@ -112,6 +114,121 @@ const ProjectCarousel = ({ screenWidth }) => {
       </Swiper>
     </div>
   );
+};
+const ProjectCarouselMobile = () => {
+  return (
+    <>
+      <div>
+        <Swiper
+          style={{
+            width: "100%",
+            paddingTop: "25px",
+            paddingBottom: "50px",
+          }}
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide
+            className="w-80 min-h-full bg-center bg-cover"
+            style={{ width: "300px", height: "300px" }}
+          >
+            <div className=" bg-[url('/project_bg_mobile/getpaid.png')] min-h-full text-center rounded-lg">
+              <div className="pt-60 text-4xl flex justify-center gap-10 text-yellow-400">
+                <a href="https://www.getpaid.asia/" target="blank">
+                  <MdOpenInNew />
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            className="w-80 min-h-full bg-center bg-cover"
+            style={{ width: "300px", height: "300px" }}
+          >
+            <div className=" bg-[url('/project_bg_mobile/appetizer.png')] min-h-full text-center rounded-lg">
+              <div className="pt-60 text-4xl flex justify-center gap-10 text-yellow-400">
+                <a
+                  href="https://github.com/D2HD/getgoing-applicationa"
+                  target="blank"
+                >
+                  <AiFillGithub />
+                </a>
+                <a href="#" target="blank">
+                  <MdOpenInNew />
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            className="w-80 min-h-full bg-center bg-cover"
+            style={{ width: "300px", height: "300px" }}
+          >
+            <div className=" bg-[url('/project_bg_mobile/ninjavan.png')] min-h-full text-center rounded-lg">
+              <div className="pt-60 text-4xl flex justify-center gap-10 text-yellow-400">
+                <a href="https://github.com/mcbebu/D2HD" target="blank">
+                  <AiFillGithub />
+                </a>
+                <a href="#" target="blank">
+                  <MdOpenInNew />
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide
+            className="w-80 min-h-full bg-center bg-cover"
+            style={{ width: "300px", height: "300px" }}
+          >
+            <div className=" bg-[url('/project_bg_mobile/csit.png')] min-h-full text-center rounded-lg">
+              <div className="pt-60 text-4xl flex justify-center gap-10 text-yellow-400">
+                <a
+                  href="https://github.com/EternalDoritos/RoomBookingApplication"
+                  target="blank"
+                >
+                  <AiFillGithub />
+                </a>
+                <a href="#" target="blank">
+                  <MdOpenInNew />
+                </a>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            className="w-80 min-h-full bg-center bg-cover"
+            style={{ width: "300px", height: "300px" }}
+          >
+            <div className=" bg-slate-500 min-h-full text-center rounded-lg">
+              <div className=" pt-32">
+                <h1 className=" text-3xl font-semibold text-teal-500">
+                  Coming Soon...
+                </h1>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+const ProjectCarousel = ({ screenWidth }) => {
+  const isInitialRequestLessThan700 = useMediaQuery("(min-width:700px)");
+
+  if (!isInitialRequestLessThan700) {
+    return <ProjectCarouselMobile />;
+  } else {
+    return <ProjectCarouselWeb />;
+  }
 };
 
 export default ProjectCarousel;
